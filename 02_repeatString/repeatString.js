4e3e24060bb8b1943b73ac25e9bb3adb285c1e9f
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-04-11 20:38:05 +0700

    done exercise javascript introduction 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(_str, n) {
+    if(n < 0)
+        return "ERROR";
 
+    let output = "";
+    for(let i = 0; i < n; i++) {
+        output += _str;
+    }
+    return output;
 };
 
 // Do not edit below this line
