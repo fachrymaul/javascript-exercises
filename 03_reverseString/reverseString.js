fc35ba7805192b73c4a4e997e8b51a4cd4344ea8
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-04-11 21:15:14 +0700

    done exercise javascript intro 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let output = "";
+    const length = string.length;
+    for(let i = 0; i < length; i++) {
+        output = string[i] + output;
+    }
 
+    return output;
 };
 
 // Do not edit below this line
