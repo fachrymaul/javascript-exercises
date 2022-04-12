430023eaabc7e58f46ed9d125ed637f454a6c526
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-04-12 21:24:47 +0700

    done excercise javascript introduction 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0)
+        return "ERROR";
 
+    if(typeof(end) != "number")
+        return "ERROR";
+
+    let sum = 0;
+    const min = Math.min(start, end);
+    const max = Math.max(start, end);
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
