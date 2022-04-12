9645406db6fdd0ea788c3a036bfe06be7f9eb7bf
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-04-12 21:10:26 +0700

    done exercise javascript intro 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 1; j < arguments.length; j++) {
+            if(array[i] === arguments[j]) {
+                array.splice(i, 1);
+                i = 0;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
