20c232e89f2618e87b66121108f909de39ca6d47
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-05-22 14:50:27 +0700

    done excercise intro javascript 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(input) {
+    let name;
+    let age = 0;
 
+    for(let i = 0; i < input.length; i++) {
+        let temp;
+        if(input[i].yearOfDeath == null) {
+            let now = new Date();
+            temp = now.getFullYear() - input[i].yearOfBirth
+        } else {
+            temp = input[i].yearOfDeath - input[i].yearOfBirth
+        }
+            
+        console.log("value of temp is " + temp)
+        if(temp > age) {
+            age = temp;
+            name = input[i];
+        }
+    }
+    return name;
 };
 
 // Do not edit below this line
