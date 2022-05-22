e23b7f13d5eb8e5e20b9c0227c111fc483d522e3
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-05-22 14:03:28 +0700

    done excercise javascript 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if(input < 0)
+        return "OOPS"
 
+    let result = [];
+    for(let i = 0; i < input; i++) {
+        if(i < 2) {
+            result.push(1);
+            continue;
+        }
+        result.push(result[i - 1] + result[i - 2]);
+    }
+
+    return result[input - 1];
 };
 
 // Do not edit below this line
