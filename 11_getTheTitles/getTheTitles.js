94c1b6af7d9a06bd01eb2f7494af8388789a36b8
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-05-22 14:21:06 +0700

    done excersice intro javascript 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(input) {
+    let result = [];
+    for(let i = 0; i < input.length; i++) {
+        result.push(input[i].title);
+    }
 
+    console.log(result);
+
+    return result;
 };
 
 // Do not edit below this line
