96bc29d1361bb54cdec1b0816c529b459dd54282
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-05-22 13:54:23 +0700

    done javascript exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const lower = input.toLowerCase();
+    const regex = /[^a-z]/g;
+    const alphabet = lower.replace(regex, "");
+    let reverse = [];
 
+    for(let i = 0; i < alphabet.length; i++) {
+        reverse.push(alphabet[alphabet.length - i - 1])
+    }
+    if(reverse.join("") == alphabet)
+        return true;
+
+    return false;
 };
 
 // Do not edit below this line
