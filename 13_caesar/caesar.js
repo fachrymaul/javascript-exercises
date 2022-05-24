2b624765c1a3b0b674a1c2f0acbdfae67b5cc6cd
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-05-25 06:03:11 +0700

    done excersice intro javascript 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
+const caesar = function(str, shift) {
+    let cypher = ""
 
+    shift = shift % 26
+
+    if(shift < 0) {
+        shift = shift + 26
+    }
+
+    for(let i = 0; i < str.length; i++) {
+        if(str[i].match(/[^a-zA-Z]/gi)) {
+            cypher += str[i]
+            continue
+        }
+
+        if(str[i] === str[i].toUpperCase()) {
+            cypher += String.fromCharCode(((str.charCodeAt(i) + shift - 65) % 26) + 65)
+        } else if(str[i] === str[i].toLowerCase()){
+            cypher += String.fromCharCode(((str.charCodeAt(i) + shift - 97) % 26) + 97)
+        }
+    }
+
+    return cypher
 };
 
 // Do not edit below this line
